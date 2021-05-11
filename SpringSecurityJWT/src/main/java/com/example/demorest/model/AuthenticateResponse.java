7ee1ff280b9a01e43c43509475297b67e36e9acb
Selvam <Selvam@DESKTOP-IF7J5KN>
2021-05-11 12:22:04 +0530

    Spring Security JWT

---
+++ SpringSecurityJWT/src/main/java/com/example/demorest/model/AuthenticateResponse.java
@@ -0,0 +1,16 @@
+package com.example.demorest.model;
+
+public class AuthenticateResponse {
+	
+	private final String jwt;
+
+	public AuthenticateResponse(String jwt) {
+		this.jwt = jwt;
+	}
+	
+	public String getJwt() {
+		return jwt;
+	}
+	
+
+}
